a3e57d9bbc14a7ab64282fef8c3df5683a2b83a4
MichaelWkTran <Michael.wktran@gmail.com>
2023-10-13 22:44:54 +1300

    Added Json Saving

--- Assets/Save System/SaveSystem.cs
+++ Assets/Save System/SaveSystem.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 //Note that this system is only loading and saving data to m_data and does not interact with external information
 public static class SaveSystem
 {
-    const string m_fileName = "/Save.bb";
+    const string m_fileName = "Save.json";//"/Save.bb";
 
     [Serializable] public class SaveData
     {
@@ -30,42 +30,55 @@ public static class SaveSystem
         public float m_musicVolume;
         public int m_qualityLevel;
     }
+    static bool m_isLoaded = false;
     public static SaveData m_data = new SaveData();
 
     //Saves the data from m_data to the file, Application.persistentDataPath + m_fileName
     public static void Save()
     {
+        if (!m_isLoaded) return;
+        
         //Get the file directory of the save data
         string path = Application.persistentDataPath + m_fileName;
         
-        //Create and open file stream
-        FileStream stream = new FileStream(path, FileMode.Create);
+        //Load from Json
+        string saveDataJson = JsonUtility.ToJson(m_data);
+        File.WriteAllText(path, saveDataJson);
 
-        //Create binary formatter and serialize the game data
-        BinaryFormatter formatter = new BinaryFormatter();
-        formatter.Serialize(stream, m_data);
-
-        //Close the file stream
-        stream.Close();
+        ////Create and open file stream
+        //FileStream stream = new FileStream(path, FileMode.Create);
+        //
+        ////Create binary formatter and serialize the game data
+        //BinaryFormatter formatter = new BinaryFormatter();
+        //formatter.Serialize(stream, m_data);
+        //
+        ////Close the file stream
+        //stream.Close();
     }
 
     //Loads the data stored in Application.persistentDataPath + m_fileName to m_data
     public static void Load()
     {
+        m_isLoaded = true;
+        
         //Get the file directory of the save data
         string path = Application.persistentDataPath + m_fileName;
-
+        
         //Check whether the file in the searched path exists, if not then exit the function
         if (!File.Exists(path)) { Debug.LogError("Save file not found in " + path); return; }
-
-        //Create and open file stream
-        FileStream stream = new FileStream(path, FileMode.Open);
-
-        //Create binary formatter and deserialize the game data
-        BinaryFormatter formatter = new BinaryFormatter();
-        m_data = formatter.Deserialize(stream) as SaveData;
-
-        //Close the file stream
-        stream.Close();
+        
+        //Load to Json
+        string saveDataJson = File.ReadAllText(path);
+        m_data = JsonUtility.FromJson<SaveData>(saveDataJson);
+        
+        ////Create and open file stream
+        //FileStream stream = new FileStream(path, FileMode.Open);
+        //
+        ////Create binary formatter and deserialize the game data
+        //BinaryFormatter formatter = new BinaryFormatter();
+        //m_data = formatter.Deserialize(stream) as SaveData;
+        //
+        ////Close the file stream
+        //stream.Close();
     }
 }
